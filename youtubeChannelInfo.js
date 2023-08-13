4c99e25f22b8ec7b068d9453893f9c229ff8fbe7
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-08-13 00:57:33 -0600

    Update youtubeChannelInfo.js
    adapted youtubeChannelInfo to work with a request with an array so it can get multiple channels at a time.
    
    switched from using fetch and thens with async and awaits for clearer code

--- youtubeChannelInfo.js
+++ youtubeChannelInfo.js
@@ -3,47 +3,63 @@ import pretty from 'pretty';
 function parseViewers(viewerString) {
     let numArray = Array.from(viewerString);
     let followerNumArray = [];
+
     for (let i = 0; i < numArray.length; i++) {
         if (!isNaN(numArray[i])) {
             followerNumArray.push(numArray[i]);
         }
     }
+
     let viewersNum = parseInt(followerNumArray.join(''));
+
     return viewersNum;
 }
 
 export function youtubeChannelInfo(channels) {
-    return new Promise((resolve, reject) => {
-        // Fetch data from the URL
-        fetch(`https://youtube.com/@${channels[0]}`)
-        .then(response => response.text())
-        .then(html => {
-            let prettied = pretty(html); 
-            let infoObject = {
-                displayName: '',
-                profileImageURL: '',
-                verified: false,
-                live: false,
-                title: '',
-                viewers: 0,
-            }
-            if (prettied.match(/"style": "BADGE_STYLE_TYPE_VERIFIED"/)) infoObject.verified = true;
-            if (prettied.match(/"text": "LIVE"/)) {
-                infoObject.live = true
+    return new Promise(async (resolve, reject) => {
+        try {
+            // Fetch data from the URL
+            async function getInfo(channelName) {
+                let response = await fetch(`https://youtube.com/@${channelName}`);
+                let html = await response.text();
+                let prettied = pretty(html); 
 
-                let matchedViewers = prettied.match(/"viewCountText"\s*:\s*{\s*"runs"\s*:\s*\[\s*{\s*"text"\s*:\s*"([^"]+)"/);
-                infoObject.viewers = parseViewers(matchedViewers[1]);
-            };
-            if (prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)) {
-                infoObject.profileImageURL = prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)[1];
-            };
-            if (prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)) {
-                infoObject.displayName = prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)[1];
-            };
-            resolve([infoObject])
-        })
-        .catch(err => {
-            reject(err);
-        });
+                let infoObject = {
+                    displayName: '',
+                    profileImageURL: '',
+                    verified: false,
+                    live: false,
+                    viewers: 0,
+                }
+
+                if (prettied.match(/"style": "BADGE_STYLE_TYPE_VERIFIED"/)) infoObject.verified = true;
+
+                if (prettied.match(/"text": "LIVE"/)) {
+                    infoObject.live = true
+                    let matchedViewers = prettied.match(/"viewCountText"\s*:\s*{\s*"runs"\s*:\s*\[\s*{\s*"text"\s*:\s*"([^"]+)"/);
+                    infoObject.viewers = parseViewers(matchedViewers[1]);
+                };
+
+                if (prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)) {
+                    infoObject.profileImageURL = prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)[1];
+                };
+
+                if (prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)) {
+                    infoObject.displayName = prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)[1];
+                };
+
+                return infoObject;
+            }
+            
+            let newDataArray = [];
+            for (let i = 0; i < channels.length; i++) {
+                let newInfoObject = await getInfo(channels[i]);
+                newDataArray.push(newInfoObject);
+            }
+
+            resolve(newDataArray);
+        } catch(err) {
+            reject(err)
+        } 
     });
 }
