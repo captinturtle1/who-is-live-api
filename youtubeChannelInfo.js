5f29cb7bca93b5b2c6b59d1b69a67a7ebfb3412f
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-08-12 22:59:33 -0600

    Update youtubeChannelInfo.js
    added viewer number to the Youtube API response

--- youtubeChannelInfo.js
+++ youtubeChannelInfo.js
@@ -1,5 +1,17 @@
 import pretty from 'pretty';
 
+function parseViewers(viewerString) {
+    let numArray = Array.from(viewerString);
+    let followerNumArray = [];
+    for (let i = 0; i < numArray.length; i++) {
+        if (!isNaN(numArray[i])) {
+            followerNumArray.push(numArray[i]);
+        }
+    }
+    let viewersNum = parseInt(followerNumArray.join(''));
+    return viewersNum;
+}
+
 export function youtubeChannelInfo(channels) {
     return new Promise((resolve, reject) => {
         // Fetch data from the URL
@@ -9,19 +21,21 @@ export function youtubeChannelInfo(channels) {
             let prettied = pretty(html); 
             let infoObject = {
                 displayName: '',
-                imageURL: '',
+                profileImageURL: '',
                 verified: false,
-                followers: 0,
                 live: false,
-                viewers: 0,
                 title: '',
-                catagory: '',
-                tags: [],
+                viewers: 0,
             }
+            if (prettied.match(/"style": "BADGE_STYLE_TYPE_VERIFIED"/)) infoObject.verified = true;
+            if (prettied.match(/"text": "LIVE"/)) {
+                infoObject.live = true
 
-            if (prettied.match(/"text": "LIVE"/)) infoObject.live = true;
+                let matchedViewers = prettied.match(/"viewCountText"\s*:\s*{\s*"runs"\s*:\s*\[\s*{\s*"text"\s*:\s*"([^"]+)"/);
+                infoObject.viewers = parseViewers(matchedViewers[1]);
+            };
             if (prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)) {
-                infoObject.imageURL = prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)[1];
+                infoObject.profileImageURL = prettied.match(/<link rel="image_src" href="([^"]*)"[^>]*>/)[1];
             };
             if (prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)) {
                 infoObject.displayName = prettied.match(/<meta property="og:title" content="([^"]*)"[^>]*>/)[1];
