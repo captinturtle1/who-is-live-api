807ba8a336ae65845b3c8e465b0080a047203fbe
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2023-08-16 00:17:49 -0600

    Update twitchChannelInfo.js
    refactored code to reduce duplicate code, and to be consistent with other API functions

--- twitchChannelInfo.js
+++ twitchChannelInfo.js
@@ -3,24 +3,58 @@ import 'dotenv/config';
 let accessToken = '';
 
 // get inital access_token
-fetch('https://id.twitch.tv/oauth2/token', {
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },    
-    body: new URLSearchParams({
-        'client_id': process.env.TWITCH_CLIENT_ID,
-        'client_secret': process.env.TWITCH_CLIENT_SECRET,
-        'grant_type': 'client_credentials'
-    })
-})
-.then((response) => response.json())
-.then((data) => accessToken = data.access_token)
-.catch(console.log);
+function getAccessToken() {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let options = {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },    
+                body: new URLSearchParams({
+                    'client_id': process.env.TWITCH_CLIENT_ID,
+                    'client_secret': process.env.TWITCH_CLIENT_SECRET,
+                    'grant_type': 'client_credentials'
+                })
+            };
+            
+            let response = await fetch('https://id.twitch.tv/oauth2/token', options);
+            response = await response.json();
+            console.log('token set', response);
+            accessToken = response.access_token;
+            resolve();
+        } catch(err) {
+            reject(err);
+        }
+    });
+}
+getAccessToken();
+
+function fetchTwitch(urlParems) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let options = {
+                method: "GET",
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken,
+                    'Client-Id': process.env.TWITCH_CLIENT_ID
+                }
+            };
+
+            let response = await fetch(`https://api.twitch.tv/helix/streams?${urlParems}`, options);
+            response = await response.json();
+
+            resolve(response);
+        } catch(err) {
+            reject(err);
+        }
+    });
+}
 
 export function twitchChannelInfo(channels) {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
             try {
+                // constructing twitch api url
                 let urlParems = '';
                 for (let i = 0; i < channels.length; i++) {
                     if (i == 0) {
@@ -29,62 +63,29 @@ export function twitchChannelInfo(channels) {
                         urlParems = urlParems.concat('&user_login=', channels[i]);
                     }
                 }
-                fetch(`https://api.twitch.tv/helix/streams?${urlParems}`, {
-                    method: "GET",
-                    headers: {
-                        'Authorization': 'Bearer ' + accessToken,
-                        'Client-Id': process.env.TWITCH_CLIENT_ID
-                    }
-                })
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.error) {
-                        console.log('had error, getting new token')
-                        // if token is invalid, get a new token
-                        fetch('https://id.twitch.tv/oauth2/token', {
-                            method: 'POST',
-                            headers:{
-                                'Content-Type': 'application/x-www-form-urlencoded'
-                            },    
-                            body: new URLSearchParams({
-                                'client_id': process.env.TWITCH_CLIENT_ID,
-                                'client_secret': process.env.TWITCH_CLIENT_SECRET,
-                                'grant_type': 'client_credentials'
-                            })
-                        })
-                        .then((response) => response.json())
-                        .then((data) => {
-                            // setting new token
-                            accessToken = data.access_token;
-                            // trying to get streams again with new token set
-                            fetch(`https://api.twitch.tv/helix/streams?${urlParems}`, {
-                                method: "GET",
-                                headers: {
-                                    'Authorization': 'Bearer ' + accessToken,
-                                    'Client-Id': process.env.TWITCH_CLIENT_ID
-                                }
-                            })
-                            .then((response) => response.json())
-                            .then((data) => {
-                                if (data.error) {
-                                    reject();
-                                } else {
-                                    resolve(data);
-                                }
-                            })
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            reject();
-                        })
+
+                let response = await fetchTwitch(urlParems);
+
+                // check if api gave an error, and get a new access token if so
+                if (response.error) {
+                    console.log('had error, getting new token');
+
+                    // if token is invalid, get a new token and fetch again
+                    await getAccessToken();
+                    let response = await fetchTwitch(urlParems);
+
+                    // check if api gave an error, and reject if so
+                    if (response.error) {
+                        reject(response);
                     } else {
-                        // if token is good
-                        resolve(data);
+                        resolve(response);
                     }
-                })
+                } else {
+                    // if token is already good
+                    resolve(response);
+                }
             } catch(err) {
-                console.log(err);
-                reject();
+                reject(err);
             }
     });
 }
